0cfee76cfc76f814dcecf4fbf50ee2df97ddc454
NgocLong341 <111973392+NgocLong341@users.noreply.github.com>
2023-02-11 09:57:39 +0700

    Update Testing_System_Assignment_2.sql

--- MySQL/Testing_System_Assignment_2.sql
+++ MySQL/Testing_System_Assignment_2.sql
@@ -22,37 +22,37 @@ Create Table `Department`
 
 Create Table `Position`
 (
-	PositionID 			Tinyint Unsigned Auto_Increment Primary Key,
+	PositionID 		Tinyint Unsigned Auto_Increment Primary Key,
 	PositionName 		Enum('Dev','Test','Scrum Master','PM') Not Null Unique Key
 );
 
 Create Table `Account`
 (
-	AccountID 			Tinyint Unsigned Auto_Increment Primary Key,
-	Email 				Varchar(50) Not Null Unique key,
-	UserName 			Varchar(50) Not Null Unique Key,
-	FullName 			Varchar(50) Not Null,
+	AccountID 		Tinyint Unsigned Auto_Increment Primary Key,
+	Email 			Varchar(50) Not Null Unique key,
+	UserName 		Varchar(50) Not Null Unique Key,
+	FullName 		Varchar(50) Not Null,
 	DepartmentID 		Tinyint Unsigned Not Null,
-	PositionID 			Tinyint Unsigned Not Null,
-	CreateDate 			Datetime Default Now(),
-    Foreign Key(DepartmentID) References Department(DepartmentID),
+	PositionID 		Tinyint Unsigned Not Null,
+	CreateDate 		Datetime Default Now(),
+        Foreign Key(DepartmentID) References Department(DepartmentID),
 	Foreign Key(PositionID) References `Position`(PositionID)
 );
 
 Create Table `Group`
 (
 	GroupID				Tinyint unsigned auto_increment primary key,
-    GroupName			Varchar(60) Not Null unique key,
-    CreatorID			Tinyint Unsigned Not Null,
-    CreateDate			Datetime Default Now()
+    	GroupName			Varchar(60) Not Null unique key,
+	CreatorID			Tinyint Unsigned Not Null,
+	CreateDate			Datetime Default Now()
 );
 
 Create Table `GroupAccount`
 (
 	GroupID				Tinyint Unsigned Not Null,
-    AccountID			Tinyint Unsigned Not Null,
-    JoinDate			Datetime Default Now(),
-    Primary Key(GroupID,AccountID),
+	AccountID			Tinyint Unsigned Not Null,
+	JoinDate			Datetime Default Now(),
+	Primary Key(GroupID,AccountID),
 	Foreign Key(GroupID) References `Group`(GroupID),
 	Foreign Key(AccountID) References `Account`(AccountID)
 );
@@ -60,25 +60,25 @@ Create Table `GroupAccount`
 Create Table `TypeQuestion`
 (
 	TypeID				Tinyint Unsigned Auto_Increment Primary Key,
-    TypeName			Enum('Essay','Multiple-Choice') Not Null Unique Key
+	TypeName			Enum('Essay','Multiple-Choice') Not Null Unique Key
 );
 
 
 Create Table `CategoryQuestion`
 (
 	CategoryID			Tinyint Unsigned Auto_Increment Primary Key,
-    CategoryName		Nvarchar(50) Not Null Unique Key
+	CategoryName		Nvarchar(50) Not Null Unique Key
 );
 
 Create Table `Question`
 (
 	QuestionID			Tinyint Unsigned Auto_Increment Primary Key,
-    Content				Nvarchar(100) Not Null,
-    CategoryID			Tinyint Unsigned Not Null,
-    TypeID				Tinyint Unsigned Not Null,
-    CreatorID			Tinyint Unsigned Not Null,
-    CreateDate			Datetime Default Now(),
-    Foreign Key(CategoryID) References CategoryQuestion(CategoryID),
+	Content				Nvarchar(100) Not Null,
+	CategoryID			Tinyint Unsigned Not Null,
+   	TypeID				Tinyint Unsigned Not Null,
+    	CreatorID			Tinyint Unsigned Not Null,
+    	CreateDate			Datetime Default Now(),
+    	Foreign Key(CategoryID) References CategoryQuestion(CategoryID),
 	Foreign Key(TypeID) References TypeQuestion(TypeID),
 	Foreign Key(CreatorID) References `Account`(AccountId)
 );
@@ -86,32 +86,32 @@ Create Table `Question`
 Create Table `Answer`
 (
 	AnswerID			Tinyint Unsigned Auto_Increment Primary Key,
-    Content				Nvarchar(100) Not Null,
-    QuestionID			Tinyint Unsigned Not Null,
-    IsCorrect 			Bit Default 1,
-    Foreign Key(QuestionID) References Question(QuestionID)
+    	Content				Nvarchar(100) Not Null,
+    	QuestionID			Tinyint Unsigned Not Null,
+    	IsCorrect 			Bit Default 1,
+    	Foreign Key(QuestionID) References Question(QuestionID)
 );
 
 Create Table `Exam`
 (
 	ExamID				Tinyint Unsigned Auto_Increment Primary Key,
-    `Code`				Char(10) Not Null,
-    Title				Nvarchar(50) Not Null,
-    CategoryID			Tinyint Unsigned Not Null,
-    Duration			Tinyint Unsigned Not Null,
-    CreatorID			Tinyint Unsigned Not Null,
-    CreateDate			Datetime Default Now(),
-    Foreign Key(CategoryID) References CategoryQuestion(CategoryID),
-    Foreign Key(CreatorID) References `Account`(AccountID)
+    	`Code`				Char(10) Not Null,
+    	Title				Nvarchar(50) Not Null,
+    	CategoryID			Tinyint Unsigned Not Null,
+    	Duration			Tinyint Unsigned Not Null,
+    	CreatorID			Tinyint Unsigned Not Null,
+    	CreateDate			Datetime Default Now(),
+    	Foreign Key(CategoryID) References CategoryQuestion(CategoryID),
+    	Foreign Key(CreatorID) References `Account`(AccountID)
 );
 
 Create Table `ExamQuestion`
 (
 	ExamID				Tinyint Unsigned Not Null,
-    QuestionID			Tinyint Unsigned Not Null,
-    Primary Key(ExamID,QuestionID),
-    Foreign Key(QuestionID) References Question(QuestionID),
-    Foreign Key(ExamID) References Exam(ExamID)
+    	QuestionID			Tinyint Unsigned Not Null,
+    	Primary Key(ExamID,QuestionID),
+    	Foreign Key(QuestionID) References Question(QuestionID),
+    	Foreign Key(ExamID) References Exam(ExamID)
 );
 
 
@@ -128,39 +128,39 @@ Values
 	('Giám Đốc'		),
 	('Thư Ký'		),
 	('Lễ Tân'		),
-    ('Bán Hàng'		)
+    	('Bán Hàng'		)
 ;                        
 
 Insert Into `Position`(PositionName)
 Values
 	('Dev'			),
-    ('Test'			),
-    ('Scrum Master'	),
-    ('PM'			)
+    	('Test'			),
+    	('Scrum Master'	),
+    	('PM'			)
 ;
 
 Insert Into `Account`(Email, UserName, FullName, DepartmentID, PositionID, CreateDate)
 Values
 	('Email1@gmail.com'	, 'UserName1'	, 'FullName1'	, 5	, 1, '2020-03-05'),
-    ('Email2@gmail.com'	, 'UserName2'	, 'FullName2'	, 1	, 2, '2020-03-05'),
-    ('Email3@gmail.com'	, 'UserName3'	, 'FullName3'	, 2	, 2, '2020-03-07'),
-    ('Email4@gmail.com'	, 'UserName4'	, 'FullName4'	, 3	, 4, '2020-03-08'),
-    ('Email5@gmail.com'	, 'UserName5'	, 'FullName5'	, 4	, 4, '2020-03-10'),
+    	('Email2@gmail.com'	, 'UserName2'	, 'FullName2'	, 1	, 2, '2020-03-05'),
+    	('Email3@gmail.com'	, 'UserName3'	, 'FullName3'	, 2	, 2, '2020-03-07'),
+    	('Email4@gmail.com'	, 'UserName4'	, 'FullName4'	, 3	, 4, '2020-03-08'),
+    	('Email5@gmail.com'	, 'UserName5'	, 'FullName5'	, 4	, 4, '2020-03-10'),
 	('Email6@gmail.com'	, 'UserName6'	, 'FullName6'	, 6	, 3, '2020-04-05'),
-	('Email7@gmail.com'	, 'UserName7'	, 'FullName7'	, 2	, 2, Null 		 ),
+	('Email7@gmail.com'	, 'UserName7'	, 'FullName7'	, 2	, 2, Null 	 ),
 	('Email8@gmail.com'	, 'UserName8'	, 'FullName8'	, 8	, 1, '2020-04-07'),
 	('Email9@gmail.com'	, 'UserName9'	, 'FullName9'	, 2	, 2, '2020-04-07'),
-	('Email10@gmail.com', 'UserName10'	, 'FullName10'	, 10, 1, '2020-04-09'),
-    ('Email11@gmail.com', 'UserName11'	, 'FullName11'	, 10, 1, Default	 ),
-	('Email12@gmail.com', 'UserName12'	, 'FullName12'	, 10, 1, Default	 )
+	('Email10@gmail.com'	, 'UserName10'	, 'FullName10'	, 10, 1	, '2020-04-09'	 ),
+    	('Email11@gmail.com'	, 'UserName11'	, 'FullName11'	, 10, 1	, Default	 ),
+	('Email12@gmail.com'	, 'UserName12'	, 'FullName12'	, 10, 1	, Default	 )
 ;
 
 Insert Into `Group`(GroupName, CreatorID, CreateDate)
 Values
 	('Testing System'	, 5	, '2019-03-05'),
-    ('Development'		, 1	, '2020-03-07'),
-    ('VTI Sale 01'		, 2	, '2020-03-09'),
-    ('VTI Sale 02'		, 3	, '2020-03-10'),
+    	('Development'		, 1	, '2020-03-07'),
+    	('VTI Sale 01'		, 2	, '2020-03-09'),
+    	('VTI Sale 02'		, 3	, '2020-03-10'),
     ('VTI Sale 03'		, 4	, '2020-03-28'),
     ('VTI Creator'		, 6	, '2020-04-06'),
     ('VTI Marketing 01'	, 7	, '2020-04-07'),
